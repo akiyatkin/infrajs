96c5cc620261e5cebc4443ed64f0ea110a96b5ad
akiyatkin <aky@list.ru>
2014-05-30 01:49:13 +0400

    fix float cost and infra_hash

--- infra/ext/once.php
+++ infra/ext/once.php
@@ -3,16 +3,16 @@ global $infra_once;
 $infra_once=array();
 function infra_hash($args,$r=false){
 	//Функция которая передаётся аргументом надо передвать как use
-	/*if(is_array($args)){
+	if(is_array($args)){
 		$a=array();
 		foreach($args as $k=>$v){
 			$a[$k]=infra_hash($v,true);
 		}
 	}else{
-		if(is_callable($args))$a='func!';
+		if(is_callable($args))$a='func!';//Заглушка для функции
 		else $a=$args;
 	}
-	if($r)return serialize($a);*/
+	if($r)return serialize($a);
 	return md5(serialize($a));
 }
 function &infra_once($name,$call,$args=array(),$re=false){
