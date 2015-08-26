85cc1aa043ef768bb62fe23d167736d71168172e
Антон Кияткин <aky@list.ru>
2015-08-26 12:08:36 +0400

    autofocus адаптация под модальные окна бутстрапа

--- ext/autofocus.js
+++ ext/autofocus.js
@@ -1,41 +1,48 @@
 //autofocus:(bool),//Выставлять фокус на первый пустой input в слое
+infrajs.autofocus_moveCaretToEnd=function(inp){
+	try{
+		if (inp.createTextRange){
+			var r = inp.createTextRange();
+			r.collapse(false);
+			r.select();
+		}else if (inp.selectionStart){
+			var end = inp.value.length;
+			inp.setSelectionRange(end,end);
+			inp.focus();
+		}else if(typeof(inp.selectionStart) == "number"){
+			inp.selectionStart = inp.selectionEnd = inp.value.length;
+		}
+	}catch(e){}
+}
 infrajs.autofocus=function(layer){//onshow
-	if(!layer.autofocus)return;
-	var div=$('#'+layer.div);
-	div.find('input, textarea').focus(function(){
-		var inp=$(this);
-		layer.autofocuswas=inp.attr('name');	
-	});
-	var moveCaretToEnd=function(inp){
-		try{
-			if (inp.createTextRange){
-				var r = inp.createTextRange();
-				r.collapse(false);
-				r.select();
-			}else if (inp.selectionStart){
-				var end = inp.value.length;
-				inp.setSelectionRange(end,end);
-				inp.focus();
-			}else if(typeof(inp.selectionStart) == "number"){
-				inp.selectionStart = inp.selectionEnd = inp.value.length;
-			}
-		}catch(e){}
+	if( typeof(layer)=='string' ) {
+		var div=$(layer);
+		var layer={};
+	} else {
+		if(!layer.autofocus)return;
+		var div=$('#'+layer.div);
+		div.find('input, textarea').focus(function(){
+			var inp=$(this);
+			layer.autofocuswas=inp.attr('name');	
+		});
 	}
 
 	if(layer.autofocuswas){
 		var inp=div.find('[name="'+layer.autofocuswas+'"]');
-		inp.focus();
+		
 	}else{
-		var inp=div.find('input:first[value=""][type=text]');
-		if(inp.length){
-			inp.focus();
-		}else{
-			var inp=div.find('input:first[type=text]');
-			inp.focus();
+		var inp=$('[autofocus]:first');
+		if (!inp.length) {
+			var inp=div.find('input:first[value=""][type=text]'); //На первый пустой инпут
+			if(!inp.length){
+				var inp=div.find('input:first[type=text]');
+			}
 		}
 	}
+	//inp.prop('autofocus',true);
+	inp.focus();
 	if(inp.length){
-		moveCaretToEnd(inp.get(0));
+		infrajs.autofocus_moveCaretToEnd(inp.get(0));
 	}
 }
 infrajs.autofocussave=function(layer){//oncheck
